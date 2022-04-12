b612f1e236cbef69cf89f0de26a8a410ac65ba38
newUser <nikolaygv@mail.ru>
2022-04-12 06:35:22 +0300

    Lkz lp

--- Zapisi na zanyatii.py
+++ Zapisi na zanyatii.py
@@ -2002,5 +2002,200 @@ import os.path
 # acc.withdrow_money(3000)
 # # acc.__del__()
 
-p=sqrt(5/3)
-print(p)
+#
+# class UserDate:
+#     def __init__(self, fio, old, ps, weight):
+#         # self.verify_fio(fio)
+#         # self.verify_old(old)
+#         # self.verify_weight(weight)
+#         # self.verify_ps(ps)
+#         #
+#         # self.__fio = fio.split()
+#         # self.__old = old
+#         # self.__password = ps
+#         # self.__weight = weight
+#
+#
+#         self.fio = fio
+#         self.old = old
+#         self.password = ps
+#         self.weight = weight
+#
+#     @classmethod
+#     def verify_fio(cls, fio):
+#         if not isinstance(fio, str):
+#             raise TypeError('ФИО должно быть строкой')
+#         f = fio.split()
+#         if len(f) != 3:
+#             raise TypeError('Неверный формат ФИО')
+#         letters = ''.join(re.findall(r'[a-zа-яё-]+', fio, flags=re.IGNORECASE))
+#         for s in f:
+#             if s not in letters:
+#                 raise TypeError('Можно использовать только буквы и дефис')
+#
+#     @classmethod
+#     def verify_old(cls, old):
+#         if not isinstance(old, int) or old < 14 or old > 100:
+#             raise TypeError('Возраст должен быть числом в диапазоне от 14 до 100 лет')
+#
+#     @classmethod
+#     def verify_weight(cls, weight):
+#         if not isinstance(weight, (int, float)) or weight < 14 or weight > 1100:
+#             raise TypeError('Возраст должен быть числом в диапазоне от 14 до 100 лет')
+#
+#     @classmethod
+#     def verify_ps(cls, ps):
+#         if not isinstance(ps, str):
+#             raise TypeError('Паспорт должен быть строкой')
+#         s = ps.split()
+#         if len(s) != 2 or len(s[0]) != 4 or len(s[1]) != 6:
+#             raise TypeError('Неверный формат данных')
+#         for p in s:
+#             if not p.isdigit():
+#                 raise TypeError('Буквы недопустимы в серии и номере паспорта.')
+#
+#     @property
+#     def fio(self):
+#         return self.__fio
+#
+#     @fio.setter
+#     def fio(self, fio):
+#         self.verify_fio(fio)
+#         self.__fio = fio
+#         print(self.__fio)
+#
+#     @property
+#     def old(self):
+#         return self.__old
+#
+#     @old.setter
+#     def old(self,old):
+#         self.verify_old(old)
+#         self.__old=old
+#         print(self.__old)
+#
+#     @property
+#     def password(self):
+#         return self.__password
+#
+#     @password.setter
+#     def password(self,ps):
+#         self.verify_ps(ps)
+#         self.__password=ps
+#         print(self.__password)
+#
+#
+#     @property
+#     def weight(self):
+#         return self.__weight
+#
+#     @weight.setter
+#     def weight(self,w):
+#         self.verify_weight(w)
+#         self.__weight=w
+#         print(self.__weight)
+#
+# p1 = UserDate('Глубоков Николай Николаевич', 28, '1234 567890', 105.0)
+#
+# print(p1.__dict__)
+# p1.old=30
+# p1.password='1345 999900'
+# p1.weight=90
+# print(p1.__dict__)
+#
+# class Point:
+#     def __init__(self, x=0, y=0):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __str__(self):
+#         return f'({self.__x}, {self.__y})'
+#
+#
+# class Prop:
+#     def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
+#         print("Инициализатор базового класса Prop")
+#         self._sp = sp
+#         self._ep = ep
+#         self._color = color
+#         self._width = width
+#
+#
+# class Line(Prop):
+#     def __init__(self,*args):
+#         print('Переопределенный инициализатор Line')
+#         super().__init__(*args)
+#         self.__width=5
+#
+#     def draw_line(self):
+#         print(f'Рисование линии: {self._sp},{self._ep},{self._color},{self._width} ')
+#
+#
+# line = Line(Point(1, 2), Point(10, 20))
+# line.draw_line()
+#
+#
+# class Rect(Prop):
+#
+#     def draw_line(self):
+#         print(f'Рисование линии: {self._sp},{self._ep},{self._color},{self._width} ')
+#
+#
+# rect = Rect(Point(23, 34), Point(45, 56))
+# rect.draw_line()
+# print(line._width)
+# print(issubclass(Line,Point))
+# print(issubclass(Point, object))
+# print(line.__dict__)
+class Figure:
+    def __init__(self, color):
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, c):
+        self.__color = c
+
+
+class Rectangle(Figure):
+    def __init__(self, w, h, color):
+        super().__init__(color)
+        self.__width = w
+        self.__height = h
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        if h > 0:
+            self.__height = h
+        else:
+            raise ValueError
+
+    def area(self):
+        return self.__width * self.__height
+
+
+rect = Rectangle(10, 20, "green")
+print(rect.width)
+print(rect.height)
+print(rect.color)
+rect.color = "red"
+print(rect.color)
+print(rect.area())
